d3a8026d481ab28c0af0986fcce1325f40072fc8
v0ncent <83373495+v0ncent@users.noreply.github.com>
2024-01-26 21:40:02 -0600

    more boilerplate. you can thank me later team

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,8 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lib.DashboardConfiguration;
 
-public class Climber extends SubsystemBase {
+public class Climber extends SubsystemBase implements DashboardConfiguration{
     
     @Override
     public void periodic() {
@@ -10,5 +11,9 @@ public class Climber extends SubsystemBase {
   
     @Override
     public void simulationPeriodic() {
+    }
+
+    @Override
+    public void configureDashboard() {
     }
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,8 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lib.DashboardConfiguration;
 
-public class Intake extends SubsystemBase {
+public class Intake extends SubsystemBase implements DashboardConfiguration{
     
     @Override
     public void periodic() {
@@ -10,5 +11,11 @@ public class Intake extends SubsystemBase {
   
     @Override
     public void simulationPeriodic() {
+    }
+
+    @Override
+    public void configureDashboard() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'configureDashboard'");
     }
 }
