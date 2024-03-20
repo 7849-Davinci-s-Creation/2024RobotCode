763499d6f72ef4f8cbb4f83ac7ec0724f60b98fc
PlasmaWisp <154473765+PlasmaWisp@users.noreply.github.com>
2024-03-20 15:48:12 -0500

    shuffleboard save

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -10,8 +11,8 @@ import lib.DashboardConfiguration;
 
 public class Intake extends SubsystemBase implements DashboardConfiguration {
     private final TalonSRX intakeMotor = new TalonSRX(Constants.MotorConstants.INTAKE_MOTOR);
-    // DigitalInout hasNote = new DigitalInput(Constants.IntakeConstants.BEAM_BREAKER_PORT);
-    boolean hasNote = false; // TODO: change this to digital input when beam break is implemented
+    DigitalInput hasNote = new DigitalInput(Constants.IntakeConstants.BEAM_BREAKER_PORT);
+    //boolean hasNote = false; // TODO: change this to digital input when beam break is implemented
 
     public Intake() {
         intakeMotor.setInverted(true);
@@ -26,7 +27,7 @@ public class Intake extends SubsystemBase implements DashboardConfiguration {
     }
 
     public boolean getNoteState() {
-        return this.hasNote;
+        return this.hasNote.get();
     }
 
     @Override
